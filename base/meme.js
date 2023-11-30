232ce4ac6e3c19c317c249149969447ddccbb894
ayomide <mikaheelayomide5@gmail.com>
2023-11-30 08:37:50 +0100

    showing the meme

--- base/meme.js
+++ base/meme.js
@@ -1,6 +1,19 @@
+let genBtn = document.querySelector("#genbtn");
+let img = document.querySelector("#img");
+let author = document.querySelector("#author");
+
+genBtn.addEventListener("click", fetchRandomMemes);
+
 async function fetchRandomMemes() {
   const response = await fetch('https://meme-api.com/gimme/wholesomememes');
     const data = await response.json();
-  console.log(data)
+  // console.log(data)
+  img.src = data.url;
+  author.innerHtml = data.author;
 }
-fetchRandomMemes();
+//fetchRandomMemes();
+/*
+npx tailwindcss -i ./dist/meme.css -o ./base/input.css --watch
+*/
+
+
