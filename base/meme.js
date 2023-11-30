31a4dd80d51825cbc2c6d9fb9776a2793be5431f
ayomide <mikaheelayomide5@gmail.com>
2023-11-30 09:25:23 +0100

    done

--- base/meme.js
+++ base/meme.js
@@ -1,19 +1,18 @@
 let genBtn = document.querySelector("#genbtn");
 let img = document.querySelector("#img");
 let author = document.querySelector("#author");
+let title = document.querySelector("#title");
 
 genBtn.addEventListener("click", fetchRandomMemes);
 
 async function fetchRandomMemes() {
   const response = await fetch('https://meme-api.com/gimme/wholesomememes');
     const data = await response.json();
-  // console.log(data)
-  img.src = data.url;
-  author.innerHtml = data.author;
+      console.log(data)
+      img.src = data.url;
+    author.textContent = `Authored by -- ${data.author}`;
+  title.innerHTML = `Title: ${data.title}`
 }
-//fetchRandomMemes();
-/*
-npx tailwindcss -i ./dist/meme.css -o ./base/input.css --watch
-*/
+fetchRandomMemes();
 
 
